03341977a002c29b31a48b0eb868449ddf06961d
Soonwook Hwang <hwang@kisti.re.kr>
2022-12-10 13:22:10 +0900

    Updated neuron_conda_setup.sh

--- bin/neuron_conda_setup.sh
+++ bin/neuron_conda_setup.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# conda init
-/apps/applications/miniconda3/bin/conda init
-
 USER=$(whoami)
 WORK_DIR=/scratch/$USER
 CONDA_DIR=$WORK_DIR/miniconda3
+BASH_FILE=~/.bashrc
 
-# export CONDA ENVS and PKGS
-export CONDA_ENVS_PATH=$CONDA_DIR/envs
-export CONDA_PKGS_DIRS=$CONDA_DIR/pkgs
+if [[ $(grep "conda initialize" $BASH_FILE) ]] ; then
+    echo "CONDA setup already initialized"
+    return 0
+else
+    # conda init
+    /apps/applications/miniconda3/bin/conda init
+
+    # export CONDA ENVS and PKGS PATH environment
+    export CONDA_ENVS_PATH=$CONDA_DIR/envs
+    export CONDA_PKGS_DIRS=$CONDA_DIR/pkgs    
+fi
 
 # set conda path
-source ~/.bashrc
+source $BASH_FILE
 
 conda config --set auto_activate_base False 
