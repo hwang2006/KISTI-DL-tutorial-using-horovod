89c47a730ec2ed5cdc1759fae2749d0b168eadc7
Soonwook Hwang <84169368+hwang2006@users.noreply.github.com>
2023-02-13 15:00:27 +0900

    Update singularity_horovod_batch.sh

--- bin/singularity_horovod_batch.sh
+++ bin/singularity_horovod_batch.sh
@@ -5,10 +5,10 @@
 #SBATCH -p amd_a100nv_8 # partition name (queue or class)
 #SBATCH --nodes=2 # the number of nodes
 #SBATCH --ntasks-per-node=8 # number of tasks per node
+#SBATCH --gres=gpu:8 # number of GPUs per node
 #SBATCH --cpus-per-task=4 # number of cpus per task
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
-#SBATCH --gres=gpu:8 # number of GPUs per node
 
 module load singularity/3.9.7
 
