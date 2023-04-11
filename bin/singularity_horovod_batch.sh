04a20decce2e47095f3167258877d0936273243a
Soonwook Hwang <hwang@kisti.re.kr>
2023-04-11 15:46:28 +0900

    update singularity_horovod_batch.sh

--- bin/singularity_horovod_batch.sh
+++ bin/singularity_horovod_batch.sh
@@ -3,15 +3,17 @@
 #SBATCH --time=24:00:00 # walltime 
 #SBATCH --comment=pytorch # application name
 #SBATCH -p amd_a100nv_8 # partition name (queue or class)
-#SBATCH --nodes=2 # the number of nodes
-#SBATCH --ntasks-per-node=8 # number of tasks per node
-#SBATCH --gres=gpu:8 # number of GPUs per node
+#SBATCH --nodes=1 # the number of nodes
+#SBATCH --ntasks-per-node=2 # number of tasks per node
+#SBATCH --gres=gpu:2 # number of GPUs per node
 #SBATCH --cpus-per-task=4 # number of cpus per task
 #SBATCH -o %x_%j.out
 #SBATCH -e %x_%j.err
 
 module load singularity/3.9.7
 
-##srun singularity exec --nv hvd.sif python KISTI-DL-tutorial-using-horovod/src/tensorflow/tf_keras_fashion_mnist.py 
-srun singularity exec --nv /apps/applications/singularity_images/ngc/tensorflow_22.03-tf2-py3.sif \
-             python tf_keras_fashion_mnist.py
+#srun singularity exec --nv tensorflow-pytorch-horovod:tf2.10_pt1.13.sif python KISTI-DL-tutorial-using-horovod/src/tensorflow/tf_keras_fashion_mnist.py 
+#srun singularity exec --nv /apps/applications/singularity_images/ngc/tensorflow_22.03-tf2-py3.sif \
+#             python KISTI-DL-tutorial-using-horovod/src/tensorflow/tf_keras_fashion_mnist.py
+srun singularity exec --nv /apps/applications/singularity_images/ngc/pytorch_22.03-hd-py3.sif \
+             python KISTI-DL-tutorial-using-horovod/src/pytorch/pt_mnist.py
