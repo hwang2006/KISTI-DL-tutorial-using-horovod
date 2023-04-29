18268b05ef239f828316e8cbce54bd6d308ee84b
Soonwook Hwang <hwang@kisti.re.kr>
2023-04-29 10:59:21 +0900

    Updated pytorch_mnist.py

--- src/pytorch/pytorch_mnist.py
+++ src/pytorch/pytorch_mnist.py
@@ -37,8 +37,10 @@ class Net(nn.Module):
         return output
 
 
-def train(args, model, device, train_loader, optimizer, epoch):
+def train(args, model, device, train_loader, train_sampler, optimizer, epoch):
     model.train()
+    # Horovod: set epoch to sampler for shuffling.   
+    train_sampler.set_epoch(epoch)
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -48,7 +50,7 @@ def train(args, model, device, train_loader, optimizer, epoch):
         optimizer.step()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
+                epoch, batch_idx * len(data), len(train_sampler),
                 100. * batch_idx / len(train_loader), loss.item()))
             if args.dry_run:
                 break
@@ -195,7 +197,7 @@ def main():
     #                                     op= hvd.Average)
    
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
+        train(args, model, device, train_loader, train_sampler, optimizer, epoch)
         #test(model, device, test_loader)
         test(model, device, test_loader, test_sampler)
         scheduler.step()
