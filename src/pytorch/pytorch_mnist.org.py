3d078a000305e128b73d83b235a8f5bdbd0edb58
Soonwook Hwang <hwang@kisti.re.kr>
2023-05-03 16:52:04 +0900

    Initial commit

--- src/pytorch/pytorch_mnist.org.py
+++ src/pytorch/pytorch_mnist.org.py
@@ -34,7 +34,7 @@ class Net(nn.Module):
         return output
 
 
-def train(args, model, device, train_loader, train_sampler, optimizer, epoch):
+def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
@@ -45,7 +45,7 @@ def train(args, model, device, train_loader, train_sampler, optimizer, epoch):
         optimizer.step()
         if batch_idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_sampler),
+                epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
             if args.dry_run:
                 break
@@ -133,7 +133,7 @@ def main():
 
     scheduler = StepLR(optimizer, step_size=1, gamma=args.gamma)
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, train_sampler, optimizer, epoch)
+        train(args, model, device, train_loader, optimizer, epoch)
         test(model, device, test_loader)
         scheduler.step()
 
