b63a6a9994a2f5cf86aae002da68b53a69af9ad7
Soonwook Hwang <hwang@kisti.re.kr>
2023-05-25 16:58:12 +0900

    Initial commit

---
+++ src/pytorch-lightning/pytorch_mnist_lightning_v0.py
@@ -0,0 +1,163 @@
+'''
+1) Single node, multi-GPUs
+$ python pytorch_lightning_intro.py
+
+2) 2 nodes, 2 GPUs
+$ srun -N 2 -n 4 python pytorch_lightning_intro.py
+Note that num_nodes to be specified as 2 in Trainer
+
+'''
+
+
+import os
+
+import pytorch_lightning as L
+import pandas as pd
+import seaborn as sn
+import torch
+from IPython.display import display
+from pytorch_lightning.loggers import CSVLogger
+from torch import nn
+from torch.nn import functional as F
+from torch.utils.data import DataLoader, random_split
+from torchmetrics import Accuracy
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+PATH_DATASETS = os.environ.get("PATH_DATASETS", ".")
+BATCH_SIZE = 256 if torch.cuda.is_available() else 64
+
+class LitMNIST(L.LightningModule):
+    def __init__(self, data_dir=PATH_DATASETS, hidden_size=64, learning_rate=2e-4):
+        super().__init__()
+
+        # Set our init args as class attributes
+        self.data_dir = data_dir
+        self.hidden_size = hidden_size
+        self.learning_rate = learning_rate
+
+        # Hardcode some dataset specific attributes
+        self.num_classes = 10
+        self.dims = (1, 28, 28)
+        channels, width, height = self.dims
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        )
+
+        # Define PyTorch model
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(channels * width * height, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(0.1),
+            nn.Linear(hidden_size, self.num_classes),
+        )
+
+        self.val_accuracy = Accuracy(task="multiclass", num_classes=10)
+        self.test_accuracy = Accuracy(task="multiclass", num_classes=10)
+
+    def forward(self, x):
+        x = self.model(x)
+        return F.log_softmax(x, dim=1)
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        self.val_accuracy.update(preds, y)
+
+        # Calling self.log will surface up scalars for you in TensorBoard
+        self.log("val_loss", loss, prog_bar=True)
+        self.log("val_acc", self.val_accuracy, prog_bar=True)
+
+    def test_step(self, batch, batch_idx):
+        x, y = batch
+        logits = self(x)
+        loss = F.nll_loss(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        self.test_accuracy.update(preds, y)
+
+        # Calling self.log will surface up scalars for you in TensorBoard
+        self.log("test_loss", loss, prog_bar=True)
+        self.log("test_acc", self.test_accuracy, prog_bar=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+
+    ####################
+    # DATA RELATED HOOKS
+    ####################
+
+    def prepare_data(self):
+        # download
+        MNIST(self.data_dir, train=True, download=True)
+        MNIST(self.data_dir, train=False, download=True)
+
+    def setup(self, stage=None):
+        # Assign train/val datasets for use in dataloaders
+        if stage == "fit" or stage is None:
+            mnist_full = MNIST(self.data_dir, train=True, transform=self.transform)
+            self.mnist_train, self.mnist_val = random_split(mnist_full, [55000, 5000])
+
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test" or stage is None:
+            self.mnist_test = MNIST(self.data_dir, train=False, transform=self.transform)
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train, batch_size=BATCH_SIZE)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val, batch_size=BATCH_SIZE)
+
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test, batch_size=BATCH_SIZE)
+
+#model = LitMNIST()
+#trainer = L.Trainer(
+#    #accelerator="cpu",
+#    #devices=1,
+#    #strategy="ddp_spawn",
+#    #accelerator="gpu",
+#    accelerator="auto",
+#    strategy="ddp",
+#    devices=torch.cuda.device_count() if torch.cuda.is_available() else None,
+#    max_epochs=10,
+#    num_nodes=2,
+#    logger=CSVLogger(save_dir="logs/"),
+#)
+#trainer.fit(model)
+
+
+def main():
+	model = LitMNIST()
+	trainer = L.Trainer(
+    	#accelerator="cpu",
+    	#devices=1,
+    	#strategy="ddp_spawn", 
+    	#accelerator="gpu",
+    	accelerator="auto",
+    	strategy="ddp", 
+    	devices=torch.cuda.device_count() if torch.cuda.is_available() else None,
+    	max_epochs=10,
+		num_nodes=2,
+    	logger=CSVLogger(save_dir="logs/"),
+	)
+	trainer.fit(model)
+
+
+if __name__ ==  '__main__':
+	main()
