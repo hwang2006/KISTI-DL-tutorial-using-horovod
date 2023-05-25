af6774e8fd3a40af6fa2c9fe7efa653f15b72e2e
Soonwook Hwang <hwang@kisti.re.kr>
2023-05-25 17:01:55 +0900

    Initial commit

--- src/pytorch-lightning/pytorch_mnist_lightning_v0.py
+++ src/pytorch-lightning/pytorch_mnist_lightning_v0.py
@@ -153,7 +153,7 @@ def main():
     	strategy="ddp", 
     	devices=torch.cuda.device_count() if torch.cuda.is_available() else None,
     	max_epochs=10,
-		num_nodes=2,
+        num_nodes=2,
     	logger=CSVLogger(save_dir="logs/"),
 	)
 	trainer.fit(model)
